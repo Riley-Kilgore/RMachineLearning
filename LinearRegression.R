907de27a59bae78fc2b0d94ef44dabc7756c00c2
Riley Kilgore <Riley_Kilgore@Outlook.com>
2018-04-12 18:51:56 -0700

    Calculated beta values using the given matrix equation for linear regression.

--- LinearRegression.R
+++ LinearRegression.R
@@ -9,3 +9,9 @@ X <- as.matrix(Boston[-ncol(Boston)])
 intercept <- rep(1, length(y))
 X <- cbind(intercept, X)
 
+# Here we are solving for the beta values and round them to two decimal points.
+# Explanation: t(X) transposes the matrix X. %*% is matrix multiplication.
+# Because y is a vector, the resulting product will also be a vector, this vector shows correlation between different inputs and the
+# resultant values of 'y'.
+betas <- solve(t(X) %*% X) %*% t(X) %*% y
+betas <- round(betas, 2)
